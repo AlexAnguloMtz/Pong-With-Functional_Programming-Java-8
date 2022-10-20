845ee8c2c02cced24964627912f847e46f0ebaa1
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-10-20 09:21:52 -0700

    Removed dead code

--- src/main/java/gui/Board.java
+++ src/main/java/gui/Board.java
@@ -4,23 +4,9 @@ import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Board extends JFrame implements KeyListener {
+public class Board extends JFrame {
     public Board(String windowTitle) {
         super(windowTitle);
     }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
+    
 }
